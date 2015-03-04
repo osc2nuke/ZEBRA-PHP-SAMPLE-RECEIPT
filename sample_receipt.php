ef03299c4ca8dda189564a08f19df73006dacd35
osc2nuke <osc2nuke@gmail.com>
2015-03-04 17:12:17 +0100

    updated receipt to print .PCX image.

--- sample_receipt.php
+++ sample_receipt.php
@@ -9,10 +9,10 @@ printer_set_option($handle, PRINTER_MODE, "RAW"); // And raw printing mode
 $label = "
 ! U1 JOURNAL
 ! U1 SETLP 5 2 46   
-AURORA'S FABRIC SHOP\r
+ AURORA'S FABRIC SHOP\r
 ! U1 SETLP 5 0 24
-123 Castle Drive, Kingston, RI 02881\r           
-(401) 555-4CU            \r\n
+            123 Castle Drive, Kingston, RI 02881\r           
+                       (401) 555-4CU            \r\n
 ! U1 SETLP 7 0 24
 4:20 PM    Thursday, June 04, 2020   Store: 142 \r
 
@@ -28,9 +28,9 @@ Z121 60Blue Silk 10 yds@ $15.00/yd  $150.00  N  \r
 
 Z829 60Muslin    20 yds@  $1.00/yd   $20.00  Y  \r
 
-SUBTOTAL:                    $187.10     \r
-RHODE ISLAND SALES TAX 7.00%:  $2.60     \r\n
-TOTAL:                       $189.70     \n";
+       SUBTOTAL:                    $187.10     \r
+       RHODE ISLAND SALES TAX 7.00%:  $2.60     \r\n
+       TOTAL:                       $189.70     \n";
 $label .= "! 200 200 200 120 1\n";
 $label .= "PCX 0 30\n";
 
@@ -45,9 +45,9 @@ $label .= "ENDPCX.LBL\n";
 $label .= "FORM\n";
 $label .= "PRINT\n";
 $label .= "! U1 SETLP 7 1 48
-
-PLEASE BRING THIS RECEIPT TO THE CASHIER   \r
-WITH THE REST OF YOUR PURCHASES.      \r\n
+			 
+    PLEASE BRING THIS RECEIPT TO THE CASHIER   \r
+         WITH THE REST OF YOUR PURCHASES.      \r\n
 ! U1 CENTER
 ! U1 B 128 1 2 100 0 0 59285691 ST 187.10 T 2.60
 \r\n
